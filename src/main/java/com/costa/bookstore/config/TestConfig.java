797e5a484b72ff23c47ff143b2a71b66e78873bb
Sergio Costa <sergiocosta1970@gmail.com>
2021-04-02 18:39:29 -0300

    Perfil de teste implementado

---
+++ src/main/java/com/costa/bookstore/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.costa.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.costa.bookstore.service.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	//=========================================================================
+	@Bean
+	public void instanciaBaseDeDados() {
+		this.dbService.instanciaBasedeDados();
+	}
+	//=========================================================================
+	
+}
