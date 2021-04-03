850d4f54e676cfd027d594a28dd3ffb1da9ff951
Sergio Costa <sergiocosta1970@gmail.com>
2021-04-03 16:43:02 -0300

    FindAll para Categoria utilizando padrão DTO

--- src/main/java/com/costa/bookstore/service/CategoriaService.java
+++ src/main/java/com/costa/bookstore/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.costa.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,14 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repository;
 	
+	//================================================================================
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+	//================================================================================
+	public List<Categoria> findAll(){
+		return repository.findAll();
+	}
+	//================================================================================
 }
