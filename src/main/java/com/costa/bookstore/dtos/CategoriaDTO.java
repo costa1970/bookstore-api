4d5d83a7eaa78b0606385d936f6a4f9c8706a78c
Sergio Costa <sergiocosta1970@gmail.com>
2021-04-04 11:47:57 -0300

    Ajustes finalizados no beckend

--- src/main/java/com/costa/bookstore/dtos/CategoriaDTO.java
+++ src/main/java/com/costa/bookstore/dtos/CategoriaDTO.java
@@ -2,13 +2,22 @@ package com.costa.bookstore.dtos;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.costa.bookstore.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	@NotEmpty(message = "Campo nome é obrigatório!")
+	@Length(min = 4, max = 100, message = "Deve ter entre 4 e 100 caracteres!")
 	private String nome;
+	
+	@NotEmpty(message = "Campo descrição é obrigatório!")
+	@Length(min = 4, max = 200, message = "Deve ter entre 4 e 200 caracteres!")
 	private String descricao;
 	
 	//==========================================================================
